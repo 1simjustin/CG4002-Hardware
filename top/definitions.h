02f40e00b2b0336f182f94d799ca8766fdf995e9
1simjustin <1sim.justin@gmail.com>
2026-03-31 09:33:06 +0800

    Add wifi connection led monitoring

--- top/definitions.h
+++ top/definitions.h
@@ -68,6 +68,8 @@
 
 // Heartbeat Display Definitions
 #define HB_LED_PIN D3
-#define HB_BLINK_PERIOD_MS 500
+#define HB_INIT_BLINK_PERIOD_MS 1000
+#define HB_CALIB_BLINK_PERIOD_MS 500
+#define HB_WIFI_BLINK_PERIOD_MS 250
 
 #endif // DEFINITIONS_H
